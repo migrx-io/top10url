fa383d460bf1996945a5f21cc3171f0ef5306cca
anatolii2makaroff <anatolii.makaroff@gmail.com>
2024-08-13 01:16:52 -0400

    fmt

--- cmd/cli_test.go
+++ cmd/cli_test.go
@@ -1,22 +1,21 @@
 package cmd
 
 import (
-    "os"
-    "testing"
-
+	"os"
+	"testing"
 )
 
 // TestParseArgs tests the ParseArgs function.
 func TestParseArgs(t *testing.T) {
 
-    // Test case: file and topN provided
-    os.Args = []string{"cmd", "-file=testfile.txt", "-top=5"}
-    filePath, topN := ParseArgs()
-    if filePath != "testfile.txt" {
-        t.Errorf("Expected filePath 'testfile.txt', but got '%s'", filePath)
-    }
-    if topN != 5 {
-        t.Errorf("Expected topN 5, but got %d", topN)
-    }
+	// Test case: file and topN provided
+	os.Args = []string{"cmd", "-file=testfile.txt", "-top=5"}
+	filePath, topN := ParseArgs()
+	if filePath != "testfile.txt" {
+		t.Errorf("Expected filePath 'testfile.txt', but got '%s'", filePath)
+	}
+	if topN != 5 {
+		t.Errorf("Expected topN 5, but got %d", topN)
+	}
 
 }
