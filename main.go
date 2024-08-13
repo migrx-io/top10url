0fac78d5fd9ce46319ca9d8eedbf85051d3f2532
anatolii2makaroff <anatolii.makaroff@gmail.com>
2024-08-13 02:18:49 -0400

    add time log

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
-	"github.com/migrx-io/top10url/cmd"
 	"log"
+    "time"
+    "os"
+
+    "github.com/migrx-io/top10url/cmd"
+
 )
 
 func main() {
 
+    logger := log.New(os.Stderr, "", log.LstdFlags)
+
 	// Parse command-line arguments
 	filePath, topN := cmd.ParseArgs()
+
+    start := time.Now()
 
 	// Read and parse the file
 	urls, err := cmd.ProcessFile(filePath, topN)
@@ -21,5 +29,8 @@ func main() {
 	for _, url := range urls {
 		fmt.Println(url)
 	}
+
+    duration := time.Since(start)
+    logger.Printf("Successfully processed file in %v\n", duration)
 
 }
